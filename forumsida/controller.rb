2119ca632ddf35d686812279dd2b8ca929d7a55e
itggot-isac-borghed <35598522+itggot-isac-borghed@users.noreply.github.com>
2019-05-09 10:45:50 +0200

    Modul skapad med funktioner från model.rb och dokumentation påbörjad

--- forumsida/controller.rb
+++ forumsida/controller.rb
@@ -6,6 +6,8 @@ require 'securerandom'
 require_relative 'model.rb'
 enable :sessions
 
+include Model
+
 configure do
     set :secured_route, ["/saved", "/save/", "/users/update/", "/users/edit/", "/post/create/", "/discussion/create/", "/discussion/edit/", "/discussion/delete/", "/post/edit/", "/post/delete/"]
 end
@@ -19,20 +21,40 @@ before do
     end
 end
 
+# Display Landing Page
+#
 get('/') do 
     slim(:home)
 end
 
+# Displays a user's profile
+#
+# @param [Integer] :id, The ID of the profile
+# @see Model#profil
 get('/users/:id') do
     result = profil(params)
     slim(:profile, locals:{profile:result})
 end
 
+# Displays a page where a user can edit their profile
+#
+# @param [Integer] :id, The ID of the profile
+# @see Model#profil
 get('/users/edit/:id') do 
     result = profil(params)
     slim(:editprofile, locals:{profile:result})
 end
 
+# Updates a user's profile and redirects to '/' or displays error 403
+#
+# @param [Integer] :id, The ID of the profile
+# @param [String] password, The password which is to be updated
+# @param [String] password2, The password which is used for validation
+# @param [String] mail, The email which is to be updated
+# @param [String] username, The username which is to be updated
+# @session [Integer] :id, The ID of the user
+#
+# @see Model#updateprofile
 post('/users/update/:id') do 
     result = updateprofile(params, session[:id])
     if result == true
@@ -42,21 +64,44 @@ post('/users/update/:id') do
     end
 end
 
+# Displays a user's saved discussions
+#
+# @session [Integer] :id, The ID of the user
+#
+# @see Model#saved
 get('/saved') do 
     result = saved(session[:id])
     slim(:saved, locals:{saved:result})
 end
 
+# Removes a discussion from being saved and redirects to '/saved'
+#
+# @param [Integer] :id, The ID of the discussion
+# @session [Integer] :id, The ID of the user
+#
+# @see Model#deletesave
 post('/saved/delete/:id') do
     deletesave(params, session[:id])
     redirect('/saved')
 end
 
+# Saves a discussion to a user and redirects to '/discussion/:id'
+#
+# @param [Integer] :id, The ID of the discussion
+# @session [Integer] :id, The ID of the user
+#
+# @see Model#save
 post('/save/:id') do
     save(params, session[:id])
     redirect("/discussion/#{params["id"]}")
 end
 
+# Edits the information of a user's profile and redirects to 'users/:id' or displays error 403
+#
+# @param [Integer] :id, The ID of the profile
+# @session [Integer] :id, The ID of the user
+#
+# @see Model#editinfo
 post('/users/edit/:id') do 
     if params["id"].to_i != session[:id]
         halt 403
@@ -65,10 +110,18 @@ post('/users/edit/:id') do
     redirect("/users/#{params["id"]}")
 end
 
+# Display Login Page
+#
 get('/login') do
     slim(:login)
 end
 
+# Login a user and redirect to '/' or '/loginfail'
+#
+# @param [String] Username, The input username
+# @param [String] Password, The input password
+#
+# @see Model#login
 post('/login') do
     loggedin = login(params)
     if loggedin == true
@@ -78,19 +131,35 @@ post('/login') do
     end
 end
 
+# Display Loginfail Page
 get('/loginfail') do
     slim(:loginfail)
 end
 
+# Log out a user and redirect to '/'
+#
+# @session [String] :account, The name of the user
+# @session [Integer] :id, The ID of the user
+#
 post('/logout') do
     session[:account], session[:id] = nil, nil
     redirect('/')
 end
 
+# Display Signup Page
+#
 get('/signup') do
     slim(:signup)
 end
 
+# Register a user and redirect to '/login' or doesn't register the user and redirect to '/signup'
+#
+# @param [String] Username, The name of the user which is to be registered
+# @param [String] Password, The password of the user which is to be registered
+# @param [String] Password2, The password of the user which is to be registered is repeated
+# @param [String] Mail, The email of the user which is to be registered
+#
+# @see Model#register
 post('/signup') do
     signup = register(params)
     if signup == true
@@ -100,20 +169,41 @@ post('/signup') do
     end
 end
 
+# Display the categories page
+#
+# @see Model#kategorier
 get('/categories') do
     cat = kategorier()
     slim(:categories, locals:{cats:cat})
 end
 
+# Display the discussions of a category
+#
+# @param [Integer] :id, The ID of the category
+#
+# @see Model#kategori
 get('/categories/:id') do 
     disc, kat = kategori(params["id"])
     slim(:category, locals:{discs:disc, cat:kat[0]})
 end
 
+# Display the create discussions page
+#
+# param [Integer] :id, The ID of the category
+#
 get('/discussion/create/:id') do
     slim(:createdisc, locals:{id:params["id"]})
 end
 
+# Create a discussion and redirect to it
+#
+# param [Integer] :id, The ID of the category
+# param [Hash] :file, The image of the discussion
+# param [String] titel, The title of the discussion
+# param [String] info, The information of the discussion
+# session [Integer] :id, The ID of the user
+#
+# @see Model#skapadisk 
 post('/discussion/create/:id') do
     skapadisk(params, session[:id])
     redirect("/categories/#{params["id"]}")
--- forumsida/model.rb
+++ forumsida/model.rb
@@ -1,202 +1,281 @@
-def connect()
-    db = SQLite3::Database.new("db/databas.db")
-    db.results_as_hash = true
-    return db
-end
-
-def laddabild(params)
-    if params[:file]
-        filnamn = params[:file][:filename].split(".")
-        filnamn[0] = SecureRandom.hex(10)
-        @filename = "#{filnamn[0]}" + "." + "#{filnamn[1]}"
-        file = params[:file][:tempfile]
-        File.open("./public/img/#{@filename}", 'wb') do |f|
-            f.write(file.read)
-        end
-    else
-        @filename = nil
+module Model
+    # Opens the database
+    #
+    # @return a connection to the database
+    def connect()
+        db = SQLite3::Database.new("db/databas.db")
+        db.results_as_hash = true
+        return db
     end
-    return @filename
-end
 
-def profil(params)
-    db = connect()
-    profil = db.execute('SELECT * FROM Användare WHERE Id=?', params["id"])
-    return profil.first
-end
-
-def updateprofile(params, userid)
-    db = connect()
-    if params["id"] != userid
-        return false
+    # Loads an image and gives it a random name
+    #
+    # @param [Hash] params form data
+    # @option params [Hash] :file Image information
+    #
+    # @return [String] the name of the file
+    def laddabild(params)
+        if params[:file]
+            filnamn = params[:file][:filename].split(".")
+            filnamn[0] = SecureRandom.hex(10)
+            @filename = "#{filnamn[0]}" + "." + "#{filnamn[1]}"
+            file = params[:file][:tempfile]
+            File.open("./public/img/#{@filename}", 'wb') do |f|
+                f.write(file.read)
+            end
+        else
+            @filename = nil
+        end
+        return @filename
     end
-    password = db.execute('SELECT Lösenord FROM Användare WHERE Id=?', userid).first
-    if (BCrypt::Password.new(password["Lösenord"]) == params["password2"]) == true
-        if params["password"] != ""
-            db.execute('UPDATE Användare SET Lösenord=? WHERE Id=?', BCrypt::Password.create(params["password"]), userid)
-        end
-        if params["mail"] != ""
-            db.execute('UPDATE Användare SET Mail=? WHERE Id=?', params["mail"], userid)
-        end
-        if params["username"] != ""
-            db.execute('UPDATE Användare SET Namn=? WHERE Id=?', params["username"], userid)
-        end
-        return true
-    else
-        return false
+
+    # Loads the profile of a user
+    #
+    # @param [Hash] params form data
+    # @option params [Integer] id The ID of the profile
+    #
+    # @return [Hash] consisting of the profile content
+    def profil(params)
+        db = connect()
+        profil = db.execute('SELECT * FROM Användare WHERE Id=?', params["id"])
+        return profil.first
     end
-end
 
-def saved(userid)
-    db = connect()
-    followed = db.execute('SELECT Diskussioner.Id,Diskussioner.Titel FROM Diskussioner INNER JOIN Användare_Diskussioner 
-        WHERE Användare_Diskussioner.DiskId = Diskussioner.Id AND Användare_Diskussioner.AnvId = ?', userid)
-    return followed
-end
-
-def deletesave(params, userid)
-    db = connect()
-    db.execute('DELETE FROM Användare_Diskussioner WHERE AnvId=? AND DiskId=?', userid, params["id"])
-end
-
-def save(params, userid)
-    db = connect
-    db.execute('INSERT INTO Användare_Diskussioner(AnvId, DiskId) VALUES(?, ?)', userid, params["id"])
-end
-
-def editinfo(params, userid)
-    db = connect()
-    db.execute('UPDATE Användare SET Info=? WHERE Id=?', params["info"], userid)
-end
-
-def login(params)
-    db = connect()
-    user = db.execute('SELECT Lösenord,Id FROM Användare WHERE Namn=?', params["Username"])
-    if user.empty? == false
-        if (BCrypt::Password.new(user[0]["Lösenord"]) == params["Password"]) == true
-            session[:account], session[:id] = params["Username"], user[0]["Id"]
+    # Updates the profile of a user
+    #
+    # @param [Hash] params form data
+    # @option params [Integer] id The ID of the profile
+    # @option params [String] password The password which is to be updated
+    # @option params [String] password2 The password which is used for validation
+    # @option params [String] mail The email which is to be updated
+    # @option params [String] username The username which is to be updated
+    # @userid [Integer] The ID of the user
+    #
+    # @return [true]
+    # @return [false] if IDs do not match or password2 does not match the user's current password
+    def updateprofile(params, userid)
+        db = connect()
+        if params["id"] != userid
+            return false
+        end
+        password = db.execute('SELECT Lösenord FROM Användare WHERE Id=?', userid).first
+        if (BCrypt::Password.new(password["Lösenord"]) == params["password2"]) == true
+            if params["password"] != ""
+                db.execute('UPDATE Användare SET Lösenord=? WHERE Id=?', BCrypt::Password.create(params["password"]), userid)
+            end
+            if params["mail"] != ""
+                db.execute('UPDATE Användare SET Mail=? WHERE Id=?', params["mail"], userid)
+            end
+            if params["username"] != ""
+                db.execute('UPDATE Användare SET Namn=? WHERE Id=?', params["username"], userid)
+            end
             return true
         else
             return false
         end
-    else
-        return false
     end
-end
 
-def register(params)
-    db = connect()
-    if params["Username"] != "" && params["Password"] != "" && params["Mail"] != "" && params["Password"] == params["Password2"]
-        if db.execute('SELECT Id FROM Användare WHERE Namn=? OR Mail=?', params["Username"], params["Mail"]) != []
+    # Loads a user's saved discussions
+    #
+    # @userid [Integer] The ID of the user
+    #
+    # @return [Array] containing the discussions
+    def saved(userid)
+        db = connect()
+        followed = db.execute('SELECT Diskussioner.Id,Diskussioner.Titel FROM Diskussioner INNER JOIN Användare_Diskussioner 
+            WHERE Användare_Diskussioner.DiskId = Diskussioner.Id AND Användare_Diskussioner.AnvId = ?', userid)
+        return followed
+    end
+
+    # Removes a discussion from being saved
+    #
+    # @param [Hash] params form data
+    # @option params [Integer] id The ID of the discussion
+    # @userid [Integer] The ID of the user
+    #
+    def deletesave(params, userid)
+        db = connect()
+        db.execute('DELETE FROM Användare_Diskussioner WHERE AnvId=? AND DiskId=?', userid, params["id"])
+    end
+
+    # Saves a discussion to a user
+    #
+    # @param [Hash] params form data
+    # @option params [Integer] id The ID of the discussion
+    # @userid [Integer] The ID of the user
+    #
+    def save(params, userid)
+        db = connect
+        db.execute('INSERT INTO Användare_Diskussioner(AnvId, DiskId) VALUES(?, ?)', userid, params["id"])
+    end
+
+    # Edits the information of a user's profile
+    #
+    # @param [Hash] params form data
+    # @option params [Integer] id The ID of the profile
+    # @userid [Integer] The ID of the user
+    #
+    def editinfo(params, userid)
+        db = connect()
+        db.execute('UPDATE Användare SET Info=? WHERE Id=?', params["info"], userid)
+    end
+
+    # Login a user
+    #
+    # @param [Hash] params form data
+    # @option params [String] Username The input username
+    # @option params [String] Password The input password
+    #
+    # @return [true]
+    # @return [false] if the user doesn't exist in the database or if the credentials don't match the user
+    def login(params)
+        db = connect()
+        user = db.execute('SELECT Lösenord,Id FROM Användare WHERE Namn=?', params["Username"])
+        if user.empty? == false
+            if (BCrypt::Password.new(user[0]["Lösenord"]) == params["Password"]) == true
+                session[:account], session[:id] = params["Username"], user[0]["Id"]
+                return true
+            else
+                return false
+            end
+        else
             return false
         end
-        db.execute('INSERT INTO Användare(Namn, Lösenord, Mail) VALUES (?, ?, ?)', params["Username"], BCrypt::Password.create(params["Password"]), params["Mail"])
-        return true
-    else
-        return false
     end
-end
 
-def kategorier()
-    db = connect()
-    db.execute('SELECT * FROM Kategorier')
-end
-
-def kategori(id)
-    db = connect()
-    return db.execute('SELECT Id,Titel FROM Diskussioner WHERE KatId=?', id), db.execute('SELECT * FROM Kategorier WHERE Id=?', id)
-end
-
-def skapadisk(params, userid)
-    db = connect()
-    @filename = laddabild(params)
-    db.execute('INSERT INTO Diskussioner(ÄgarId, KatId, Titel, Info, Bild) VALUES (?, ?, ?, ?, ?)', userid, params["id"], 
-        params["titel"], params["info"], @filename)
-end
-
-def diskussion(id)
-    db = connect()
-    return [db.execute('SELECT Diskussioner.*, Användare.Namn FROM Diskussioner INNER JOIN Användare ON Diskussioner.ÄgarId = Användare.Id WHERE Diskussioner.Id=?', id), 
-        db.execute('SELECT Inlägg.*, Användare.Namn FROM Inlägg INNER JOIN Användare ON Inlägg.ÄgarId = Användare.Id WHERE Inlägg.DiskId=?', id)]
-end
-
-def redigeradisk(params, userid)
-    db = connect()
-    disk = db.execute('SELECT * FROM Diskussioner WHERE Id=? AND ÄgarId=?', params["id"], userid)
-    if disk != []
-        return disk.first
-    else
-        return false
-    end
-end
-
-def spararedigeringdisk(params, userid)
-    db = connect()
-    disk = db.execute('SELECT * FROM Diskussioner WHERE Id=? AND ÄgarId=?', params["id"], userid)
-    if disk == []
-        return false
-    else
-        if params[:file]
-            @filename = laddabild(params)
-            db.execute('UPDATE Diskussioner SET Titel=?,Info=?,Bild=? WHERE Id=?', params["Titel"], params["Info"], @filename, params["id"])
+    # Register a user
+    #
+    # @param [Hash] params form data
+    # @option params [String] Username The name of the user which is to be registered
+    # @option params [String] Password The password of the user which is to be registered
+    # @option params [String] Password2 The password of the user which is to be registered is repeated
+    # @option params [String] Mail The email of the user which is to be registered
+    #
+    # @return true
+    # @return false if the credentials weren't filled or if the user already exists
+    def register(params)
+        db = connect()
+        if params["Username"] != "" && params["Password"] != "" && params["Mail"] != "" && params["Password"] == params["Password2"]
+            if db.execute('SELECT Id FROM Användare WHERE Namn=? OR Mail=?', params["Username"], params["Mail"]) != []
+                return false
+            end
+            db.execute('INSERT INTO Användare(Namn, Lösenord, Mail) VALUES (?, ?, ?)', params["Username"], BCrypt::Password.create(params["Password"]), params["Mail"])
+            return true
         else
-            db.execute('UPDATE Diskussioner SET Titel=?, Info=? WHERE Id=?', params["Titel"], params["Info"], params["id"])
+            return false
         end
-        return disk.first["Id"]
     end
-end
 
-def tabortdisk(params, userid)
-    db = connect()
-    disk = db.execute('SELECT ÄgarId,KatId FROM Diskussioner WHERE Id=?', params["id"]).first
-    if disk.length == 0 || userid != disk["ÄgarId"]
-        return false
-    else
-        db.execute('DELETE FROM Diskussioner WHERE Id=?', params["id"])
-        db.execute('DELETE FROM Inlägg WHERE DiskId=?', params["id"])
-        return disk["KatId"]
+    # Load the categories in the database
+    #
+    # @return [Array] consisting of the categories
+    def kategorier()
+        db = connect()
+        db.execute('SELECT * FROM Kategorier').first
     end
-end
 
-def skapainlg(params, userid)
-    db = connect()
-    @filename = laddabild(params)
-    db.execute('INSERT INTO Inlägg(DiskId, ÄgarId, Info, Bild) VALUES (?, ?, ?, ?)', params["id"], userid, params["info"], @filename)
-end
-
-def redigerainlg(params, userid)
-    db = connect()
-    inlg = db.execute('SELECT * FROM Inlägg WHERE Id=? AND ÄgarId=?', params["id"], userid)
-    if inlg != []
-        return inlg.first
-    else
-        return false
+    # Load the discussions of a category
+    #
+    # @id [Integer] The ID of the category
+    #
+    # @return [Array] consisting of the discussions
+    def kategori(id)
+        db = connect()
+        return db.execute('SELECT Id,Titel FROM Diskussioner WHERE KatId=?', id), db.execute('SELECT * FROM Kategorier WHERE Id=?', id)
     end
-end
 
-def spararedigeringinlg(params, userid)
-    db = connect()
-    inlg = db.execute('SELECT * FROM Inlägg WHERE Id=? AND ÄgarId=?', params["id"], userid)
-    if inlg == []
-        return false
-    else
-        if params[:file]
-            @filename = laddabild(params)
-            db.execute('UPDATE Inlägg SET Info=?,Bild=? WHERE Id=?', params["Info"], @filename, params["id"])
+    
+    def skapadisk(params, userid)
+        db = connect()
+        @filename = laddabild(params)
+        db.execute('INSERT INTO Diskussioner(ÄgarId, KatId, Titel, Info, Bild) VALUES (?, ?, ?, ?, ?)', userid, params["id"], 
+            params["titel"], params["info"], @filename)
+    end
+
+    def diskussion(id)
+        db = connect()
+        return [db.execute('SELECT Diskussioner.*, Användare.Namn FROM Diskussioner INNER JOIN Användare ON Diskussioner.ÄgarId = Användare.Id WHERE Diskussioner.Id=?', id), 
+            db.execute('SELECT Inlägg.*, Användare.Namn FROM Inlägg INNER JOIN Användare ON Inlägg.ÄgarId = Användare.Id WHERE Inlägg.DiskId=?', id)]
+    end
+
+    def redigeradisk(params, userid)
+        db = connect()
+        disk = db.execute('SELECT * FROM Diskussioner WHERE Id=? AND ÄgarId=?', params["id"], userid)
+        if disk != []
+            return disk.first
         else
-            db.execute('UPDATE Inlägg SET Info=? WHERE Id=?', params["Info"], params["id"])
+            return false
         end
-        return inlg.first["DiskId"]
     end
-end
 
-def tabortinlg(params, userid)
-    db = connect()
-    inlg = db.execute('SELECT ÄgarId,DiskId FROM Inlägg WHERE Id=?', params["id"]).first
-    if userid != inlg["ÄgarId"]
-        return false
-    else
-        db.execute('DELETE FROM Inlägg WHERE Id=?', params["id"])
-        return inlg["DiskId"]
+    def spararedigeringdisk(params, userid)
+        db = connect()
+        disk = db.execute('SELECT * FROM Diskussioner WHERE Id=? AND ÄgarId=?', params["id"], userid)
+        if disk == []
+            return false
+        else
+            if params[:file]
+                @filename = laddabild(params)
+                db.execute('UPDATE Diskussioner SET Titel=?,Info=?,Bild=? WHERE Id=?', params["Titel"], params["Info"], @filename, params["id"])
+            else
+                db.execute('UPDATE Diskussioner SET Titel=?, Info=? WHERE Id=?', params["Titel"], params["Info"], params["id"])
+            end
+            return disk.first["Id"]
+        end
+    end
+
+    def tabortdisk(params, userid)
+        db = connect()
+        disk = db.execute('SELECT ÄgarId,KatId FROM Diskussioner WHERE Id=?', params["id"]).first
+        if disk.length == 0 || userid != disk["ÄgarId"]
+            return false
+        else
+            db.execute('DELETE FROM Diskussioner WHERE Id=?', params["id"])
+            db.execute('DELETE FROM Inlägg WHERE DiskId=?', params["id"])
+            return disk["KatId"]
+        end
+    end
+
+    def skapainlg(params, userid)
+        db = connect()
+        @filename = laddabild(params)
+        db.execute('INSERT INTO Inlägg(DiskId, ÄgarId, Info, Bild) VALUES (?, ?, ?, ?)', params["id"], userid, params["info"], @filename)
+    end
+
+    def redigerainlg(params, userid)
+        db = connect()
+        inlg = db.execute('SELECT * FROM Inlägg WHERE Id=? AND ÄgarId=?', params["id"], userid)
+        if inlg != []
+            return inlg.first
+        else
+            return false
+        end
+    end
+
+    def spararedigeringinlg(params, userid)
+        db = connect()
+        inlg = db.execute('SELECT * FROM Inlägg WHERE Id=? AND ÄgarId=?', params["id"], userid)
+        if inlg == []
+            return false
+        else
+            if params[:file]
+                @filename = laddabild(params)
+                db.execute('UPDATE Inlägg SET Info=?,Bild=? WHERE Id=?', params["Info"], @filename, params["id"])
+            else
+                db.execute('UPDATE Inlägg SET Info=? WHERE Id=?', params["Info"], params["id"])
+            end
+            return inlg.first["DiskId"]
+        end
+    end
+
+    def tabortinlg(params, userid)
+        db = connect()
+        inlg = db.execute('SELECT ÄgarId,DiskId FROM Inlägg WHERE Id=?', params["id"]).first
+        if userid != inlg["ÄgarId"]
+            return false
+        else
+            db.execute('DELETE FROM Inlägg WHERE Id=?', params["id"])
+            return inlg["DiskId"]
+        end
     end
 end
